cbd6a62ec05fefe7bed7346186bbf7628bd614de
Jennifer Browning <browningjen@yahoo.com>
2018-08-06 08:55:43 -0400

    first attempt

---
+++ index.js
@@ -0,0 +1,18 @@
+function() {
+
+    for (let i = 0; i <= 1000; i++){
+      if (i % 2 == 0 && i % 3 == 0) {
+          document. write("<div class='red bold'>" + i + "</div>");
+    } else if (i % 2 === 0) {
+        document. write("<div class='bold'>" + i + "</div>");
+    }
+      else if (i % 3 == 0) {
+             document. write("<div class='red'>" + i + "</div>");
+         }
+
+      else {
+          document. write("<div>" + i + "</div>");
+
+    }}
+    
+
